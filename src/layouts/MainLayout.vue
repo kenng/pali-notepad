cbd1b7497a65c16e6970c9a18540a60773d340a7
Ken Ng <kenng@itwonders-web.com>
2021-06-22 09:59:16 +0800

    hide left sidebar btn

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,20 +2,20 @@
     <q-layout view="lHh Lpr lFf">
         <q-header elevated>
             <q-toolbar>
-                <q-btn
+                <!-- <q-btn
                     flat
                     dense
                     round
                     icon="menu"
                     aria-label="Menu"
                     @click="leftDrawerOpen = !leftDrawerOpen"
-                />
+                /> -->
 
                 <q-toolbar-title>
-                    Quasar App
+                    Pali Easy Input
                 </q-toolbar-title>
 
-                <div>Quasar v{{ $q.version }}</div>
+                <div>v1.0.0</div>
             </q-toolbar>
         </q-header>
 
@@ -47,42 +47,6 @@ const linksData = [
         caption: 'quasar.dev',
         icon: 'school',
         link: 'https://quasar.dev',
-    },
-    {
-        title: 'Github',
-        caption: 'github.com/quasarframework',
-        icon: 'code',
-        link: 'https://github.com/quasarframework',
-    },
-    {
-        title: 'Discord Chat Channel',
-        caption: 'chat.quasar.dev',
-        icon: 'chat',
-        link: 'https://chat.quasar.dev',
-    },
-    {
-        title: 'Forum',
-        caption: 'forum.quasar.dev',
-        icon: 'record_voice_over',
-        link: 'https://forum.quasar.dev',
-    },
-    {
-        title: 'Twitter',
-        caption: '@quasarframework',
-        icon: 'rss_feed',
-        link: 'https://twitter.quasar.dev',
-    },
-    {
-        title: 'Facebook',
-        caption: '@QuasarFramework',
-        icon: 'public',
-        link: 'https://facebook.quasar.dev',
-    },
-    {
-        title: 'Quasar Awesome',
-        caption: 'Community Quasar projects',
-        icon: 'favorite',
-        link: 'https://awesome.quasar.dev',
     },
 ];
 

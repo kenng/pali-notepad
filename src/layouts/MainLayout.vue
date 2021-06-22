5962f29f92b5dd5337dda1a11df14c28daba1c86
Ken Ng <kenng@itwonders-web.com>
2021-06-22 11:20:43 +0800

    hide left nav sidebar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,7 +19,12 @@
             </q-toolbar>
         </q-header>
 
-        <q-drawer v-model="leftDrawerOpen" bordered content-class="bg-grey-1">
+        <!-- <q-drawer
+            v-model="leftDrawerOpen"
+            bordered
+            content-class="bg-grey-1"
+            draggable="false"
+        >
             <q-list>
                 <q-item-label header class="text-grey-8">
                     Essential Links
@@ -30,7 +35,7 @@
                     v-bind="link"
                 />
             </q-list>
-        </q-drawer>
+        </q-drawer> -->
 
         <q-page-container>
             <router-view />
